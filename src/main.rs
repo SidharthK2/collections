84326426e8014370c1322c16b3f079ab8b36bed2
sidharthK2 <sidharthkapoor.100@gmail.com>
2023-06-09 15:57:30 +0100

    hashmaps insert

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+use std::collections::HashMap;
+
 fn main() {
-    let s1 = String::from("नमस्ते");
-    for i in s1.chars() {
-        println!("{}", i);
+    let blue = String::from("Blue");
+    let mut scores = HashMap::new();
+    scores.insert(blue, 10);
+    scores.insert(String::from("Red"), 50);
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+    for (k, v) in &scores {
+        println!("{} {}", k, v);
     }
 }
