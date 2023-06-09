e66c58a38918b7bf0cd67aded4cfb683d9372ee2
sidharthK2 <sidharthkapoor.100@gmail.com>
2023-06-09 15:24:43 +0100

    string, utf-8

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 fn main() {
-    let mut s1 = String::from("Hello, ");
-    let s2 = String::from("world!");
-    // let s2 = "world!";
-    s1.push_str(&s2[..]);
-    let s3 = format!("{s1}-{s2}");
-    println!("s1 {} s3 {} ", s1, s3);
+    let s1 = String::from("नमस्ते");
+    for i in s1.chars() {
+        println!("{}", i);
+    }
 }
